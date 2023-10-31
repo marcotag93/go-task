d1b5816c550d68b1555c59080541f3b098718d98
Marco Tagliaferri <85807872+marcotag93@users.noreply.github.com>
2023-10-31 02:21:59 +0100

    fix filename input

--- functions/saveTrialData.m
+++ functions/saveTrialData.m
@@ -1,13 +1,14 @@
 % Define a function for saving data
 function saveTrialData(datadir, subj, runnum, trial_struct, condition)
-    filename = fullfile(datadir, sprintf('sub%02d_ses%02d_%s.mat', subj, runnum, condition));
+    filename = fullfile(datadir, sprintf('sub-%s_ses%02d_%s.mat', subj, runnum, condition));
+
 
     % Check if the file already exists
     if isfile(filename)
         % If the file already exists, append a number to the filename
         counter = 1;
         while isfile(filename)
-            filename = fullfile(datadir, sprintf('sub%02d_ses%02d_%s_%d.mat', subj, runnum, condition, counter));
+            filename = fullfile(datadir, sprintf('sub-%s_ses%02d_%s_%d.mat', subj, runnum, condition, counter));  
             counter = counter + 1;
         end
     end
